c1607049de0e2bdbe2cabb2723e6957d55901afa
Laurent Della-Negra <laurent.dellanegra@gmail.com>
2022-06-23 22:28:03 +0200

    💄 Add font and optimisation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   plugins: [require('@tailwindcss/forms'), require('@tailwindcss/aspect-ratio')],
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         primary: '#579660',
         'primary-light': '#EEF4EF',
@@ -12,5 +18,4 @@ module.exports = {
       },
     },
   },
-  plugins: [],
 }
