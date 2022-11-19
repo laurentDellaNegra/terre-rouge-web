f082912579b3558a4f79a47dc0a3ba4fee52482b
laurentDellaNegra <laurent.dellanegra@gmail.com>
2022-11-19 13:43:03 +0100

    💄 Decrease size header

--- src/components/LogoClouds/index.tsx
+++ src/components/LogoClouds/index.tsx
@@ -9,11 +9,11 @@ import vaoSuzette from '@/public/logos/partners/vao-suzette.svg'
 export default function LogoClouds() {
   return (
     <div className="bg-primary">
-      <div className="mx-auto max-w-7xl py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl py-8 px-4 sm:py-16 sm:px-6 lg:px-8">
         <TitleSection center variant="white" className="text-3xl font-bold tracking-tight">
           Nos partenaires
         </TitleSection>
-        <div className="grid grid-cols-1 xs:grid-cols-2 lg:grid-cols-4 justify-between mt-16 lg:mt-24">
+        <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-4 justify-between mt-4 lg:mt-8">
           <div className="h-44 flex items-center justify-center">
             <Image className="h-36 w-52" src={ala} alt="Logo d'Ala" />
           </div>
