c75e259c44a9ff034b070c18a9b547c9bac27846
laurentDellaNegra <laurent.dellanegra@gmail.com>
2023-01-21 21:14:31 +0100

    🐛 Delete cartid if error on fetchCart

--- src/context/ShopifyClient/getCart/useGetCart.ts
+++ src/context/ShopifyClient/getCart/useGetCart.ts
@@ -8,13 +8,14 @@ import { GetCartQuery } from './GetCartQuery'
 export const getCartKey = (id: string | null) => ['cart', id]
 
 export default function useGetCart() {
-  const { cartId } = useShopifyClient()
+  const { cartId, deleteCartId } = useShopifyClient()
   return useQuery({
     queryKey: getCartKey(cartId),
     queryFn: async () => {
       const { cart } = await graphQLRequestClient.request(GetCartQuery, { id: cartId! })
       return cart
     },
+    onError: deleteCartId,
     enabled: !!cartId,
   })
 }
